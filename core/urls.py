d63067ef0a547b57c6cf6e18835af30d246e7e38
Manjeet <43195111+Manjeete@users.noreply.github.com>
2020-06-04 16:34:00 +0530

    product page base setup

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('checkout', views.checkout, name='checkout'),
-    path('product', views.product, name='product')
+    path('checkout/', views.checkout, name='checkout'),
+    path('product/', views.product, name='product')
 
 ]
