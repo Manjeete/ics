ef8a6d0d0cb07b77407d77c1c42ba4fb2fe5e291
Manjeet <43195111+Manjeete@users.noreply.github.com>
2020-06-04 17:42:28 +0530

    reduce product-page coding

--- core/models.py
+++ core/models.py
@@ -1,9 +1,23 @@
 from django.conf import settings
 from django.db import models
 
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('P', 'primary'),
+    ('S', 'secondary'),
+    ('D', 'danger')
+)
+
 class Item(models.Model):
     title = models.CharField(max_length=120)
     price = models.FloatField()
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=2)
 
     def __str__(self):
         return self.title
